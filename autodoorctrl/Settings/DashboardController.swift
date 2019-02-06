9d11e5d51bb44771cfbb2d6af10aa407bc141c3b
Jing Wei Li <ljw9801055@gmail.com>
2019-02-05 21:39:32 -0500

    Now Able to Send Emails to RPI Fixx (#18)_

--- autodoorctrl/Settings/DashboardController.swift
+++ autodoorctrl/Settings/DashboardController.swift
@@ -15,48 +15,45 @@ class DashboardController: NSObject {
     let actionIdentifier = "dashboardActionCell"
     
     let sectionTitles = [NSLocalizedString("myInfoTitle", comment: ""),
+                         NSLocalizedString("supportTitle", comment: ""),
                          NSLocalizedString("Actions", comment: "")]
     let firstSectionCount = 2
     let secondSectionCount = 4
+    
     let actionTitles = [NSLocalizedString("ShowActiveTitle", comment: ""),
                         NSLocalizedString("registerTitle", comment: ""),
                         NSLocalizedString("AddToActiveTitle", comment: ""),
                         NSLocalizedString("removeTitle", comment: "")]
+    
+    let supportTitles = [NSLocalizedString("contactFixxTitle", comment: "")]
+    
+    lazy var sectionCounts: [Int] = {
+        return [2, supportTitles.count, actionTitles.count]
+    }()
     
     override init () {
         super.init()
     }
     
     
-    func loadCells(from indexPath: IndexPath) -> (String, String?) {
+    func loadCells(from indexPath: IndexPath) -> (String, String?, UIImage?) {
         if indexPath.section == 0 {
             if indexPath.row == 0 {
-                return ("RCSID", User.current.rcsID )
+                return ("RCSID", User.current.rcsID, nil)
             } else if indexPath.row == 1 {
-                return ("Status", User.current.isActive ? "Active" : "Inactive")
+                return ("Status", User.current.isActive ? "Active" : "Inactive", nil)
             }
-        } else {
-            return (actionTitles[indexPath.row], nil)
+        } else if indexPath.section == 1 {
+            return (supportTitles[indexPath.row], nil, UIImage(named: "wrenchIcon"))
+        } else if indexPath.section == 2 {
+            return (actionTitles[indexPath.row], nil, UIImage(named: "SettingsIcon"))
         }
-        return ("", nil)
-    }
-    
-    func handleActions(on vc: UIViewController, from indexPath: IndexPath) {
-        if indexPath.row == 0 {
-            vc.performSegue(withIdentifier: "showDashboardDetails", sender: vc)
-        } else if indexPath.row == 1 {
-            handleStudentOperations(from: .register, on: vc)
-        } else if indexPath.row == 2 {
-            handleStudentOperations(from: .addToActive, on: vc)
-        } else if indexPath.row == 3 {
-            handleStudentOperations(from: .remove, on: vc)
-        }
+        return ("", nil, nil)
     }
     
     // MARK: - Private
     
-    private func handleStudentOperations(from mode: ServicesAPI.StudentOperations,
-                                         on vc: UIViewController) {
+    func studentOperations(for mode: ServicesAPI.StudentOperations) -> UIViewController {
         let alert = UIAlertController(title: NSLocalizedString("enterRCSIDTitle", comment: ""),
                                       message: nil, preferredStyle: .alert)
         alert.addTextField { $0.placeholder = "RCSID" }
@@ -64,16 +61,14 @@ class DashboardController: NSObject {
         alert.addAction(UIAlertAction(title: NSLocalizedString("Continue", comment: ""),
                                       style: .destructive, handler: { _ in
             guard let rcsID = alert.textFields?.first?.text else { return }
-            ServicesAPI.performOperationOnStudent(with: rcsID, method: mode,
-                                                  successHandler: {
-                                                    SwiftMessagesWrapper.showSuccessMessage(title: NSLocalizedString("Message", comment: ""),
-                                                                                            body: NSLocalizedString("Done", comment: ""))
-            },
-                                                  errorHandler: { $0.handleError() })
+            ServicesAPI.performOperationOnStudent(with: rcsID, method: mode, successHandler: {
+                SwiftMessagesWrapper.showSuccessMessage(title: NSLocalizedString("Message", comment: ""),
+                                                        body: NSLocalizedString("Done", comment: ""))
+            }, errorHandler: { $0.handleError() })
         }))
         
         alert.addAction(UIAlertAction(title: NSLocalizedString("Cancel", comment: ""), style: .cancel, handler: nil))
-        vc.present(alert, animated: true, completion: nil)
+        return alert
     }
     
 }
